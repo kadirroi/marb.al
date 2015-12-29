4d395ee8933bee3babefec5426f515ea387dbd81
Oguz <oguz.eroglu@windowslive.com>
2015-12-29 15:56:00 +0200

    arduino kodlari eklendi.

---
+++ Applied Forward Kinematics/settings.py
@@ -0,0 +1,11 @@
+CAMERA_INDEX = 1
+
+COLOR_BINARIZATION_THRESHOLD = 70  # change this value according to
+                                   # segmented mode
+
+CARD_DIMENSION = 40
+
+CARDS = [
+         {'name': 'c1',
+          'color': (19.0, 28.0, 25.0)}
+        ]
